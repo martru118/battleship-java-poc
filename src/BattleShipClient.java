5ff418c2843367b9001f1a4a41c1621715efea41
CheyenneC-ontechu <cheyenne.carrier@ontariotechu.net>
2020-03-17 19:46:30 -0400

    battleship placement

--- src/BattleShipClient.java
+++ src/BattleShipClient.java
@@ -7,6 +7,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.DataInputStream;
@@ -24,37 +26,54 @@ public class BattleShipClient extends Application {
         GridPane pane = new GridPane();
         pane.setMinHeight(600);
 
-        Canvas canvas = new Canvas(1050,500);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Canvas playerCanvas = new Canvas(500,500);
+        Canvas opponentCanvas = new Canvas(500,500);
+        GraphicsContext gcP = playerCanvas.getGraphicsContext2D();
+        GraphicsContext gcO = opponentCanvas.getGraphicsContext2D();
         for (int i = 50; i <= 500; i+=50) {
-            gc.strokeLine(i,0,i,500);
-            gc.strokeLine(0,i,500,i);
+            gcP.strokeLine(i,0,i,500);
+            gcP.strokeLine(0,i,500,i);
         }
-        for (int i = 550; i <= 1050; i+=50) {
-            gc.strokeLine(i,0,i,500);
-            gc.strokeLine(550,i-550,1050,i-550);
+        for (int i = 50; i <= 500; i+=50) {
+            gcO.strokeLine(i,0,i,500);
+            gcO.strokeLine(0,i,500,i);
         }
 
         TextArea ta = new TextArea();
         TextField tf = new TextField();
 
 
-        pane.add(canvas,0,0);
-        pane.add(tf,0,1);
-        pane.add(ta,0,2);
+        pane.add(playerCanvas,0,0,1,1);
+        pane.add(opponentCanvas,1,0,1,1);
+        pane.add(tf,0,1,2,1);
+        pane.add(ta,0,2,2,1);
+        pane.setHgap(50);
         stage.setScene(new Scene(pane));
-        stage.setTitle("BattleShipClient");
+        stage.setTitle("BattleShip");
         stage.show();
+
 
         tf.setOnAction(e -> {
             try {
                 String coordinatesToFire = tf.getText();
                 toServer.writeUTF(coordinatesToFire);
 
+                int [] coordinate = new int[2];
+
+                coordinate[0] = Character.getNumericValue(tf.getText().charAt(0));
+                coordinate[1] = Character.getNumericValue(tf.getText().charAt(1));
+
                 toServer.flush();
                 String message = fromServer.readUTF();
+                boolean hit = fromServer.readBoolean();
 
                 ta.appendText(message + "\n");
+                if (hit) {
+                    gcO.setFill(Color.RED);
+                } else {
+                    gcO.setFill(Color.BLACK);
+                }
+                gcO.fillOval(coordinate[0] * 50 + 20, coordinate[1] * 50 + 20, 10,10);
 
             } catch (IOException ex) {
                 ex.printStackTrace();
--- src/BattleShipServer.java
+++ src/BattleShipServer.java
@@ -8,6 +8,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.DataInputStream;
@@ -53,7 +54,37 @@ public class BattleShipServer extends Application {
         stage.setTitle("BattleShip");
         stage.show();
 
-        new Thread(() -> {
+        Thread startingThread = new Thread(() -> {
+            Ship[] ships = new Ship[3];
+            ships[0] = new Ship(2, "Patrol");
+            ships[1] = new Ship(3,"Submarine");
+            ships[2] = new Ship(3,"Destroyer");
+
+            for (int i = 0; i < ships.length; i++) {
+                ta.appendText("Welcome to Battle ship \n" +
+                        "Please place your " + ships[i].name +" ("+ships[i].spaces+ " spaces) \n" );
+
+                for(int j = 0; j < ships[i].spaces; j++) {
+                    ta.appendText("Please type in coordinate #" + j+" out of "+ships[i].spaces+".\n");
+
+                    tf.setOnAction(e -> {
+                        int coordinates[] = new int[2];
+                        coordinates[0] = Integer.parseInt(tf.getText(0,1));
+                        coordinates[1] = Integer.parseInt(tf.getText(1,2));
+
+                        while (board[coordinates[0]][coordinates[1]]) {
+                            ta.appendText("Spot taken, try again");
+                        }
+                        board[coordinates[0]][coordinates[1]] = true;
+                        gcP.setFill(Color.AQUA);
+                        gcP.fillRect(coordinates[0]*50 + 2,coordinates[1]*50 + 2, 45,45);
+                    });
+                }
+            }
+
+        });
+
+        Thread opponentTurn = new Thread(() -> {
             try {
                 ServerSocket serverSocket = new ServerSocket(8000);
                 //server started time + date
@@ -75,17 +106,25 @@ public class BattleShipServer extends Application {
 
                     if (isHit(coordinate, board)) {
                         toClient.writeUTF("You Hit Opponent!");
+                        toClient.writeBoolean(true);
                         ta.setText("You have been hit \n");
+                        gcP.setFill(Color.RED);
                     } else {
                         toClient.writeUTF("You have missed.");
+                        toClient.writeBoolean(false);
                         ta.setText("Opponent missed \n");
+                        gcP.setFill(Color.BLACK);
                     }
+                    gcP.fillOval(coordinate[0] * 50 + 20, coordinate[1] * 50 + 20, 10,10);
                 }
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }).start();
+        });
+
+        startingThread.start();
+        opponentTurn.start();
 
 
 
@@ -95,6 +134,9 @@ public class BattleShipServer extends Application {
             return true;
         }
         return false;
+    }
+    public void welcome (TextArea ta, TextField tf) {
+
     }
 
     private class Ship {
